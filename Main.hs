d292fb395b631c82aff2b7c47217451529a5d736
Jack B <jack.goldsbrough@outlook.com>
2025-07-16 18:32:19 +0200

    first draft of setup

--- Main.hs
+++ Main.hs
@@ -10,9 +10,9 @@ main = do
     (fileName:lowerSt:upperSt:_) <- getArgs
     let lower = read lowerSt :: Int
         upper = read upperSt :: Int
+    writeFile fileName ("x-Value,y" ++ fileName ++ "\n")
     loopFromPArray fileName lower upper 
-    readProcess "runhaskell" ("toplot.hs" : words fileName) ""
-    
+    readProcess "venv\\Scripts\\python" ["graphbuddy.py"] fileName
     
     
 
@@ -22,8 +22,8 @@ loopFromPArray :: String -> Int -> Int -> IO ()
 loopFromPArray fileName n upper 
     | n == upper = do
                 let a = (fromPArray n) ! n
-                appendFile fileName (show n ++ "\t" ++ show a ++ "\n")
+                appendFile fileName (show n ++ "," ++ show a ++ "\n")
     | otherwise = do
                 let a = (fromPArray n ) ! n
-                appendFile fileName (show n ++ "\t" ++ show a ++ "\n")
+                appendFile fileName (show n ++ "," ++ show a ++ "\n")
                 loopFromPArray fileName (n+1) upper
